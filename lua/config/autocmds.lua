5bc3a1cbca4614d4f9f846c9149b1d4b1ec96c62
Troy Mitchell <TroyMitchell988@gmail.com>
2024-11-08 14:20:24 +0800

    fit linux

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -1,3 +1,13 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+vim.api.nvim_create_autocmd({ "FileType" }, {
+	pattern = { "c", "cpp", "md", "txt", "c.snippets", "cpp.snippets" },
+	callback = function()
+		vim.b.autoformat = false
+		vim.opt_local.expandtab = false
+		vim.opt_local.tabstop = 8
+		vim.opt_local.shiftwidth = 8
+		vim.opt_local.softtabstop = 8
+	end,
+})
