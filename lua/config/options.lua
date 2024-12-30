19e98315417590f9c4b35f7b9bb00bcd94de198b
Troy Mitchell <TroyMitchell988@gmail.com>
2024-12-30 14:21:00 +0800

    add comments
    Signed-off-by: Troy Mitchell <TroyMitchell988@gmail.com>

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,6 +2,8 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 local opt = vim.opt
-opt.shiftwidth = 8 -- 修改默认缩进
+-- 修改默认缩进
+opt.shiftwidth = 8
 opt.tabstop = 8
+-- 允许复制内容到剪切板
 opt.clipboard = "unnamedplus"
