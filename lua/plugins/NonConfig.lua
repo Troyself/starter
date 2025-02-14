c0e6579219525d8a4d4eb2e9e1017a1b52b82585
Troy Mitchell <TroyMitchell988@gmail.com>
2025-02-14 13:35:27 +0800

    add NonCfongi.lua to add some plugins

---
+++ lua/plugins/NonConfig.lua
@@ -0,0 +1,13 @@
+return {
+	-- 注释/反注释
+	{ "scrooloose/nerdcommenter" },
+
+	-- 浮动终端
+	{ "voldikss/vim-floaterm" },
+
+	-- markdown
+	{ "preservim/vim-markdown", lazy = true, enabled = true },
+
+	-- 彩虹括号
+	-- { "luochen1990/rainbow" },
+}
