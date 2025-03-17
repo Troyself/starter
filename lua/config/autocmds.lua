e6b7ebb1884e43a34b716124a8ae82f351baf7fd
Troy Mitchell <troymitchell988@gmail.com>
2025-03-17 15:57:09 +0800

    fix sh tab
    Signed-off-by: Troy Mitchell <troymitchell988@gmail.com>

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -15,7 +15,7 @@ vim.api.nvim_create_autocmd({ "FileType" }, {
 	pattern = { "sh" },
 	callback = function()
 		vim.b.autoformat = false
-		vim.opt_local.expandtab = false
+		vim.opt_local.expandtab = true
 		vim.opt_local.tabstop = 2
 		vim.opt_local.shiftwidth = 2
 		vim.opt_local.softtabstop = 2
--- lua/config/options.lua
+++ lua/config/options.lua
@@ -3,7 +3,7 @@
 -- Add any additional options here
 local opt = vim.opt
 -- 修改默认缩进
-opt.shiftwidth = 8
-opt.tabstop = 8
+-- opt.shiftwidth = 8
+-- opt.tabstop = 8
 -- 允许复制内容到剪切板
 opt.clipboard = "unnamedplus"
