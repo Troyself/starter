2866eb178c1ff26873c04019ef00971cc5819357
Troy Mitchell <troymitchell988@gmail.com>
2025-04-27 21:28:39 +0800

    plugin: add chatgpt
    Signed-off-by: Troy Mitchell <troymitchell988@gmail.com>

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -9,3 +9,14 @@ vim.keymap.set("n", "<leader>mlr", "<cmd>Leet last_submit<CR>", { desc = "Leet L
 vim.keymap.set("n", "<leader>mlo", "<cmd>Leet open<CR>", { desc = "Leet Open" })
 vim.keymap.set("n", "<leader>mlc", "<cmd>Leet reset<CR>", { desc = "Clear" })
 vim.keymap.set("n", "<leader>mll", "<cmd>Leet<CR>", { desc = "Leet" })
+
+-- 普通 ChatGPT 聊天窗口
+vim.keymap.set("n", "<leader>mcg", "<cmd>ChatGPT<CR>", { desc = "Open ChatGPT" })
+
+-- 当前选中的文本发送到 ChatGPT（Visual 模式下）
+vim.keymap.set(
+	"v",
+	"<leader>mce",
+	":ChatGPTEditWithInstructions<CR>",
+	{ desc = "Edit with ChatGPT" }
+)
