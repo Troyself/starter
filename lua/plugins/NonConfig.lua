8c9fb1e8924db8a07fc4b94470143e9ee9ff700d
Troy Mitchell <troymitchell988@gmail.com>
2025-04-27 21:25:46 +0800

    plugin: use vim-floaterm
    Signed-off-by: Troy Mitchell <troymitchell988@gmail.com>

--- lua/plugins/NonConfig.lua
+++ lua/plugins/NonConfig.lua
@@ -3,7 +3,13 @@ return {
 	{ "scrooloose/nerdcommenter" },
 
 	-- 浮动终端
-	{ "voldikss/vim-floaterm" },
+	--{ "voldikss/vim-floaterm" },
+	{
+		"voldikss/vim-floaterm",
+		keys = {
+			{ "<C-_>", "<Cmd>FloatermToggle<CR>", desc = "Toggle Floaterm" }, -- <C-/> 实际等于 <C-_>
+		},
+	},
 
 	-- markdown
 	{ "preservim/vim-markdown", lazy = true, enabled = true },
