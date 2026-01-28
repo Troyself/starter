9df13c56532aa36cfedf4381746acf8a9e5a1de3
Troy Mitchell <troymitchell988@gmail.com>
2026-01-28 22:01:02 +0800

    autocmd: add troy-wujie14-pro folder filter
    Signed-off-by: Troy Mitchell <troymitchell988@gmail.com>

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -21,3 +21,17 @@ vim.api.nvim_create_autocmd({ "FileType" }, {
 		vim.opt_local.softtabstop = 2
 	end,
 })
+
+vim.api.nvim_create_autocmd("FileType", {
+	pattern = { "c", "cpp", "md", "txt", "c.snippets", "cpp.snippets" },
+	callback = function()
+		local path = vim.fn.expand("%:p")
+		if path:find("/home/troy/die2die/") then
+			vim.b.autoformat = false
+			vim.opt_local.expandtab = true
+			vim.opt_local.tabstop = 4
+			vim.opt_local.shiftwidth = 4
+			vim.opt_local.softtabstop = 4
+		end
+	end,
+})
