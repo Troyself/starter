6465f449fec60c299e6ba1cd273fcc73bbde1aeb
Troy <troymitchell988@gmail.com>
2026-01-29 19:23:04 +0800

    autocmds: use space instead of tab
    Signed-off-by: Troy <troymitchell988@gmail.com>

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -5,7 +5,7 @@ vim.api.nvim_create_autocmd({ "FileType" }, {
 	pattern = { "c", "cpp", "md", "txt", "c.snippets", "cpp.snippets" },
 	callback = function()
 		vim.b.autoformat = false
-		vim.opt_local.expandtab = false
+		vim.opt_local.expandtab = true
 		vim.opt_local.tabstop = 8
 		vim.opt_local.shiftwidth = 8
 		vim.opt_local.softtabstop = 8
